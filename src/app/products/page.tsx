7a66359cac52236bf7b3d5139346eb37b233e5f1
bmichalczuk <barmic90@hotmail.com>
2024-02-13 12:49:56 +0100

    change Prdouct list items to Link

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,6 @@ import { getProductsList } from "../api/products";
 
 export default async function ProductsPage() {
 	const products = await getProductsList();
-	console.log(products);
 	return (
 		<div>
 			<h1>Products</h1>
--- src/app/ui/molecules/ProductListItem.tsx
+++ src/app/ui/molecules/ProductListItem.tsx
@@ -1,16 +1,21 @@
+import Link from "next/link";
 import type { ProductType } from "../types";
+
 import { ProductListItemImage } from "@/app/ui/atoms/ProductListItemImage";
 import { ProductListItemDescription } from "@/app/ui/atoms/ProductListItemDescription";
+
 export type ProductListItemProps = {
 	product: ProductType;
 };
 
 export const ProductListItem = ({ product }: ProductListItemProps) => {
 	return (
-		<article className="border border-gray-300 p-2 shadow-md transition-transform hover:shadow-lg">
-			<ProductListItemImage {...product.coverImage} />
+		<Link href={`/product/${product.id}`}>
+			<article className="border border-gray-300 p-2 shadow-md transition-transform hover:shadow-lg">
+				<ProductListItemImage {...product.coverImage} />
 
-			<ProductListItemDescription {...product} />
-		</article>
+				<ProductListItemDescription {...product} />
+			</article>
+		</Link>
 	);
 };
