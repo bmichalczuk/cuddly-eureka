78b276660a5cb8b84ef4895db213c58c359a79ea
bmichalczuk <barmic90@hotmail.com>
2024-02-15 23:26:00 +0100

    add generateStaticParams for single product page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,8 +1,30 @@
 import { Suspense } from "react";
+import type { Metadata } from "next";
 import { ProductDescription } from "@ui/atoms/ProductDescription";
-import { getProductById } from "@/api/products";
+import { getProductById, getProductsList } from "@/api/products";
 import { ProductCoverImage } from "@ui/atoms/ProductCoverImage";
 import { SuggestedProductsList } from "@/ui/organisms/SuggestedProductList";
+
+export const generateStaticParams = async () => {
+	const products = await getProductsList();
+	return products
+		.map((product) => {
+			return { productId: product.id };
+		})
+		.slice(0, 2);
+};
+
+export async function generateMetadata({
+	params,
+}: {
+	params: { productId: string };
+}): Promise<Metadata> {
+	const product = await getProductById(params.productId);
+
+	return {
+		title: product.name,
+	};
+}
 
 export default async function ProductPage({ params }: { params: { productId: string } }) {
 	const product = await getProductById(params.productId);
--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
-		<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
+		<section className="mx-auto max-w-md px-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
 			{children}
 		</section>
 	);
