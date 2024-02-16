a0c169e201995f77af7fd65ddbfbd0f91ae753be
bmichalczuk <barmic90@hotmail.com>
2024-02-17 00:41:42 +0100

    change h1 to div in site header cuz test fail

--- src/app/ui/organisms/SiteHeader.tsx
+++ src/app/ui/organisms/SiteHeader.tsx
@@ -3,11 +3,11 @@ import { SiteNavbar } from "../molecules/SiteNavbar";
 export function SiteHeader() {
 	return (
 		<header className="m-0 flex w-full justify-between bg-slate-700 p-7 text-white">
-			<h1 className="font-serif text-5xl italic text-white">
+			<div className="font-serif text-5xl italic text-white">
 				<Link className="text-5xl font-bold italic" title="Go to home page" href="/">
 					Cuddly<span className="text-3xl text-red-400">shop</span>
 				</Link>
-			</h1>
+			</div>
 			<SiteNavbar />
 			<div className="p-5 font-bold ">Cart</div>
 		</header>
