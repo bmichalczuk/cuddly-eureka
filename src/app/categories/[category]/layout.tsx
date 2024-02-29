62de3fde1e4eb0b4c2e8b3f0651e9c6eb05222b7
bmichalczuk <barmic90@hotmail.com>
2024-02-29 22:07:35 +0100

    --amend

--- src/app/categories/[category]/layout.tsx
+++ src/app/categories/[category]/layout.tsx
@@ -13,6 +13,8 @@ export const generateStaticParams = async () => {
 
 export default function CategoryLayout({ children }: { children: ReactNode }) {
 	return (
-		<section className="mx-auto max-w-md  sm:max-w-2xl sm:py-1 xl:max-w-6xl">{children}</section>
+		<section className="mx-auto min-h-full max-w-md px-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
+			{children}
+		</section>
 	);
 }
