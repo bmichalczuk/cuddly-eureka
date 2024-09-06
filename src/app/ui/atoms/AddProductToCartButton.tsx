0ee3d944649fa42b427814253e5a17288851c2ea
bmichalczuk <barmic90@hotmail.com>
2024-09-06 12:56:17 +0200

    add AddProductToCartButton

--- src/app/ui/atoms/AddProductToCartButton.tsx
+++ src/app/ui/atoms/AddProductToCartButton.tsx
@@ -8,7 +8,7 @@ export const AddProductToCartButton = () => {
 	return (
 		<button
 			type="submit"
-			className="w-full rounded-md border bg-slate-700 px-8 py-3 text-white disabled:cursor-wait disabled:bg-slate-300"
+			className="mt-2 w-full rounded-md border bg-slate-700 px-8 py-3 text-white hover:bg-slate-500 focus:bg-slate-500 disabled:cursor-wait disabled:bg-slate-300"
 			disabled={formStatus.pending}
 		>
 			Add to cart
