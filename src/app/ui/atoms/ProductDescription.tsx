eb76fedbbdf098f3d3bd8ed10be96bba7bfd2c9a
bmichalczuk <barmic90@hotmail.com>
2024-10-03 14:47:51 +0200

    change cattegories name in product descripton to name

--- src/app/ui/atoms/ProductDescription.tsx
+++ src/app/ui/atoms/ProductDescription.tsx
@@ -1,3 +1,5 @@
+import { type Route } from "next";
+import Link from "next/link";
 import { type ProductFragment } from "../../../gql/graphql";
 import { formatPrice } from "@/utils/utils";
 
@@ -13,7 +15,12 @@ export const ProductDescription = ({
 			<h1 className="my-5 text-center text-3xl font-bold">{name}</h1>
 			<p className="px-5">{description}</p>
 			<div className="my-3 flex flex-row justify-between px-1">
-				<div className="cursor-pointer font-serif italic hover:underline">{`Category: ${categories[0]?.name || ""}`}</div>
+				<div className="cursor-pointer font-serif italic hover:underline">
+					<Link
+						href={`/categories/${categories[0]?.name.toLowerCase()}/1` as Route}
+						title="See more products in this category"
+					>{`Category: ${categories[0]?.name || ""}`}</Link>
+				</div>
 				<div className="font-semibold " data-testid="product-price">
 					<span className="sr-only">Cena:</span>
 					{formatPrice(price)}
